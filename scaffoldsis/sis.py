55d6377c8c8ed55fa5ea43995e84b7c041670181
nickp60 <nickp60@gmail.com>
2017-11-23 14:11:23 +0000

    changed license

--- scaffoldsis/sis.py
+++ scaffoldsis/sis.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# sis.py generates scaffoldingformation from nucmer coordinates files
+# Copyright (C) 2017  Zanoni Dias, Ulisses Dias, Nicholas Waters and João C. Setubal
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
- #   This software was developed by Zanoni Dias, Ulisses Dias and João
- #   C. Setubal
- #
- #   It should not be redistributed or used for any commercial purpose
- #   without written permission from authors
- #
- #   release date: nov 15, 2011
- #
- # This software is experimental in nature and is
- # supplied "AS IS", without obligation by the authors to provide
- # accompanying services or support.  The entire risk as to the quality
- # and performance of the Software is with you. The authors
- # EXPRESSLY DISCLAIM ANY AND ALL WARRANTIES REGARDING THE SOFTWARE,
- # WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES
- # PERTAINING TO MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- #
 
 import sys
 import re
--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-"""
-A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+#!/usr/bin/env python3
+
+# setup module for scaffoldsis, also known as SIS
+# Copyright (C) 2017  Zanoni Dias, Ulisses Dias, Nicholas Waters, and João C Setubal
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from setuptools import setup, find_packages
 import re
@@ -25,7 +37,7 @@ setup(
     long_description="""
     a program to generate draft genome sequence scaffolds for prokaryotes.
     https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-13-96
-    by Zanoni Dias, Ulisses Dias and João C Setubal
+    by Zanoni Dias, Ulisses Dias, Nicholas Waters, and João C Setubal
     """,
     url='https://github.com/nickp60/SIS',
     install_requires=['biopython'],
@@ -34,9 +46,9 @@ setup(
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Programming Language :: Python :: 3',
     ],
     keywords='bioinformatics',
